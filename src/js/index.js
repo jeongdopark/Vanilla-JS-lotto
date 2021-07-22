691ec192ff5f600ecc9415e3b4fbd916bad5e56c
jeongdopark <jdpark0816@gmail.com>
2021-07-22 18:23:56 +0900

    fix : inputSwitch 위치를 수정하여 1000원 이하일 경우 발생하는 error 해결

--- src/js/index.js
+++ src/js/index.js
@@ -17,11 +17,11 @@ let inputSwitch = "False"
 let toggleCheckedValue = $lottoNumbersToggleButton.checked     // toggle의 boolean값 
 
 
-    if(toggleCheckedValue){         // toggle에 따른 초기값 설정
-      ticket_UI_Wrap.style.flexDirection = "column"
-    }else{
-      ticket_UI_Wrap.style.flexDirection = "row"
-    }
+    // if(toggleCheckedValue){         // toggle에 따른 초기값 설정
+    //   ticket_UI_Wrap.style.flexDirection = "column"
+    // }else{
+    //   ticket_UI_Wrap.style.flexDirection = "row"
+    // }
 
 
 for(let i = 0; i < mt_9.length; i++){       // modal 부분은 display none 처리
@@ -33,7 +33,7 @@ btn.addEventListener('click', () =>{        // 버튼 click
   console.log(inputSwitch);
     if(inputSwitch === "False"){      // 변수 inputSwitch를 통해서 티켓구매 여부를 체크 
       clickOrEnterEvent()             // inputSwitch가 "True"인 상태면 더이상 click이나 keydown을 통해 clickOrEnterEvent()함수가 실행 안된다
-      inputSwitch = "True"
+
     }else{
       return true
     }
@@ -42,11 +42,9 @@ btn.addEventListener('click', () =>{        // 버튼 click
 
 document.addEventListener('keydown', (e) =>{        // enter 버튼 
   const keycode = e.keyCode;
-  console.log(inputSwitch);
   if(keycode === 13 && inputSwitch === "False"){
     e.preventDefault()        // preventDefault() 없을 경우 새로고침됨
     clickOrEnterEvent()
-    inputSwitch = "True"
   }if(keycode === 13 && inputSwitch === "True"){      // elif가 왜 안되는건지
     e.preventDefault()
   }
@@ -59,8 +57,9 @@ function clickOrEnterEvent(num){
     const change = money % 1000   // 입력 값을 1000으로 나누고 나머지를 구한다. (거스름 돈)
     if(money < 1000 || money == ""){            // 최소 금액을 1000원으로 설정 & 아무 값이 없을 경우 경고창 발생
       window.alert("1000원 이상을 입력해주세요")
-      
+
     }else{
+      inputSwitch = "True"
       window.alert(`${count}장 구매완료 . 거스름돈 : ${change}원`)
       displayBlock(ticketInfo)
       ticketAmountInfo(count)
@@ -78,6 +77,7 @@ function ticketAmountInfo(amount){    // 구매 개수를 알려주는 function
 function displayBlock(c){        // display none 이었던 section을 display block으로 변경하는 function
   c.style.display = 'block'
 }
+
 
 
 function createTicketUI(count){      // 구매 개수만큼 ticket UI를 나타내는 function
